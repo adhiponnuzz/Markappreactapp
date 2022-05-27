157c86a384033f706573fe16c160c1de6a229d1e
Adhiponnuzz <adithyaminnu689693@gmail.com>
2022-05-27 15:29:29 +0530

    search finished

--- src/component/Search.js
+++ src/component/Search.js
@@ -3,6 +3,22 @@ import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const Search = () => {
+    const deleteApicall=(id)=>{
+        const data={"_id":id}
+        console.log(data)
+        axios.post("http://localhost:5000/api/delete",data).then((Response)=>{
+            if(Response.data.status=="success")
+            {
+                alert("successfully deleted")
+            }
+            else{
+                alert("error in deletion")
+            }
+        })
+    }
+      
+        
+    
     var [admno,setAdmno]=useState("")
     const [data,setData]=useState([{"name":"","cgpa":""}])
 
@@ -32,7 +48,7 @@ const Search = () => {
             </div>
             {data.map((value,key)=>{
 
-                return <div className="row">
+                return <div className="row g-3">
                  <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" >
                    <label for="" className="form-label">Name</label>
                    <input type="text" value={value.name} className="form-control"/>
@@ -42,6 +58,9 @@ const Search = () => {
                 <label for="" className="form-label">Cgpa</label>
                 <input type="text" value={value.cgpa} className="form-control"/>
                 
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                  <button onClick={()=>{deleteApicall(value._id)}}  className="btn btn-success">DELETE</button>
                 </div>
                 </div>
                
